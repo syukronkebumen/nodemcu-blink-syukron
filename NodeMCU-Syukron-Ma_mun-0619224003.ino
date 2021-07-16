2b5443a77ecba45df48d4827e05456d078cc8a13
syukron_mamun <37414066+syukronkebumen@users.noreply.github.com>
2021-07-16 23:38:41 +0700

    Add files via upload

---
+++ NodeMCU-Syukron-Ma_mun-0619224003.ino
@@ -0,0 +1,18 @@
+#include <ESP8266WiFi.h>;
+#include <BlynkSimpleEsp8266.h>;
+
+#define BLYNK_PRINT Serial
+ 
+char auth[] = "ZoOMvMXgwwFP1GmGVG6bZnZNM0JF9Pwi"; //token blynk via e-mail
+ 
+char ssid[] = "aaa12345"; //nama wifi
+char pass[] = "bismillah123#"; //password wifi
+ 
+void setup(){
+  Serial.begin(9600);
+  Blynk.begin(auth, ssid, pass);
+}
+ 
+void loop(){
+  Blynk.run();
+}
